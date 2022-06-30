3abd0e2583ba839d38b4f7d7a809551732a7f8d6
Jean-Frederic Clere <jfclere@gmail.com>
2022-06-30 17:08:59 +0200

    get a basic tomcat in podman running, add war to test.

--- test/native/files/start.sh
+++ test/native/files/start.sh
@@ -15,21 +15,45 @@ then
 fi
 
 
-sed -i "s/8080/${tomcat_port}/" ./conf/server.xml
+if [ ! -z ${tomcat_port} ]; then
+  sed -i "s/8080/${tomcat_port}/" ./conf/server.xml
+fi
 
-sed -i "s/8005/${tomcat_shutdown_port}/" ./conf/server.xml
+if [ ! -z ${tomcat_shutdown_port} ]; then
+  sed -i "s/8005/${tomcat_shutdown_port}/" ./conf/server.xml
+fi
 
-sed -i "s/8009/${tomcat_ajp_port}/" ./conf/server.xml
+if [ ! -z ${tomcat_ajp_port} ]; then
+  sed -i "s/8009/${tomcat_ajp_port}/" ./conf/server.xml
+fi
+if [ ! -z ${tomcat_address} ]; then
+  sed -i "s/127.0.0.1/${tomcat_address}/" ./conf/server.xml
+fi
 
-mv  *.war webapps/ || true
-
-if ((${tomcat_port}==0)); then
-    sed -i "s/changeMe/${tomcat_ajp_port}/" ./conf/server.xml
+if [ -z ${tomcat_port} ]; then
+  if [ ! -z ${tomcat_ajp_port} ]; then
+    sed -i "s/s/changeMe/${tomcat_ajp_port}/" ./conf/server.xml
+  else
+    sed -i "s/changeMe/8009/" ./conf/server.xml
+  fi
 else
     sed -i "s/changeMe/${tomcat_port}/" ./conf/server.xml
 fi
 
-sed -i "s/6666/${cluster_port}/" ./conf/server.xml
+
+if [ ${cluster_port}==0 ]; then
+  cluster_port=6666
+fi
+sed -i "s/proxyport/${cluster_port}/" ./conf/server.xml
+sed -i "s/proxyaddress/127.0.0.1/" ./conf/server.xml
+
+echo "jvm_route: ${jvm_route} and tomcat_port: ${tomcat_port}"
+if [ ! -z ${jvm_route} ]; then
+  sed -i "/<Engine name=\"Catalina\"/c\<Engine name=\"Catalina\" defaultHost=\"localhost\" jvmRoute=\"${jvm_route}\">" ./conf/server.xml
+fi
+
+# copy webapp war file.
+mv  *.war webapps/ || true
 
 catalina.sh run
 
