2b945c7218231763e4a628fb0cbd7fbbdde9aeb6
Vladimír Chlup <vchlup@redhat.com>
2023-07-24 11:36:10 +0200

    test: Minor tweaks, don't use missing function

--- test/basetests.sh
+++ test/basetests.sh
@@ -16,7 +16,7 @@ docker cp testapp tomcat8081:/usr/local/tomcat/webapps
 sleep 10
 
 # Basic 200 and 404 tests.
-echotestlabel "basic 200 and 404 tests"
+echo "basic 200 and 404 tests"
 CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8000/testapp/test.jsp)
 if [ ${CODE} != "200" ]; then
   echo "Failed can't reach webapp: ${CODE}"
@@ -39,7 +39,7 @@ do
    nohup docker run --network=host -e tomcat_port=8080 --name tomcat8080 ${IMG} &
    sleep 10
    tomcat_wait_for_n_nodes 1 || clean_and_exit
-   docker exec -it tomcat8080 /usr/local/tomcat/bin/shutdown.sh
+   docker exec tomcat8080 /usr/local/tomcat/bin/shutdown.sh
    tomcat_wait_for_n_nodes 0 || clean_and_exit
    docker container rm tomcat8080
    iter=$(expr $iter + 1)
