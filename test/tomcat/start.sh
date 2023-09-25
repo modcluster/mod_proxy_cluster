2f9f86c8226631deb2baeb8b588915a0965b8d80
Vladimír Chlup <vchlup@redhat.com>
2023-09-25 16:09:31 +0200

    Use portOffset for tomcat containers, update tomcat_start function

--- test/tomcat/start.sh
+++ test/tomcat/start.sh
@@ -1,53 +1,15 @@
 #!/bin/bash
 
-if [ "$tomcat_shutdown_port" = true ] 
-then
-    while :
-    do
-        port=$(shuf -i 8000-9000 -n 1)
-        if [[ $(ss -tulpn | grep $port) ]] || [[ "$port" = $tomcat_port ]] || [[ "$port" = $tomcat_ajp_port ]]; then
-            echo "Port $port is used"
-        else
-            tomcat_shutdown_port=$port
-            break
-        fi
-    done
-fi
-
-
-if [ ! -z ${tomcat_port} ]; then
-  sed -i "s/tomcat_port/${tomcat_port}/" ./conf/server.xml
-fi
-
-if [ ! -z ${tomcat_shutdown_port} ]; then
-  sed -i "s/shutdown_port/${tomcat_shutdown_port}/" ./conf/server.xml
-fi
-
-if [ ! -z ${tomcat_ajp_port} ]; then
-  sed -i "s/ajp_port/${tomcat_ajp_port}/" ./conf/server.xml
-fi
 if [ ! -z ${tomcat_address} ]; then
   sed -i "s/127.0.0.1/${tomcat_address}/" ./conf/server.xml
 fi
 
-if [ -z ${tomcat_port} ]; then
-  if [ ! -z ${tomcat_ajp_port} ]; then
-    sed -i "s/changeMe/${tomcat_ajp_port}/" ./conf/server.xml
-  else
-    sed -i "s/changeMe/8009/" ./conf/server.xml
-  fi
-else
-    sed -i "s/changeMe/${tomcat_port}/" ./conf/server.xml
-fi
+sed -i "s/port_offset/${tomcat_port_offset:-0}/g" ./conf/server.xml
+sed -i "s/proxyport/${cluster_port:-6666}/" ./conf/server.xml
 
+conPort=$(expr 8080 + ${tomcat_port_offset:-0})
+sed -i "s/changeMe/$conPort/" ./conf/server.xml
 
-if [ "${cluster_port}" -eq "0" ]; then
-  cluster_port=6666
-fi
-sed -i "s/proxyport/${cluster_port}/" ./conf/server.xml
-sed -i "s/proxyaddress/127.0.0.1/" ./conf/server.xml
-
-echo "jvm_route: ${jvm_route:-default} tomcat_port: ${tomcat_port:-default} tomcat_address: ${tomcat_address:-default} tomcat_ajp_port: ${tomcat_ajp_port:-default} tomcat_shutdown_port: ${tomcat_shutdown_port:-default}"
 if [ ! -z ${jvm_route} ]; then
   sed -i "/<Engine name=\"Catalina\"/c\<Engine name=\"Catalina\" defaultHost=\"localhost\" jvmRoute=\"${jvm_route}\">" ./conf/server.xml
 fi
