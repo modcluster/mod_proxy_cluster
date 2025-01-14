77e5726585837e16b8c430437f6154aeaf746ce9
Vladimír Chlup <vchlup@redhat.com>
2025-01-14 10:34:48 +0100

    ci: Generate coverage report for the testsuite
    Install gcc package that provides gcovr

--- test/testsuite.sh
+++ test/testsuite.sh
@@ -128,11 +128,14 @@ if [ $CODE_COVERAGE ]; then
     echo "Generating test coverage..."
     httpd_start > /dev/null 2>&1
     docker exec $MPC_NAME /usr/local/apache2/bin/apachectl stop
+
     for f in $(ls coverage/*.json); do
         docker cp -q $f $MPC_NAME:/coverage/
     done
+
     docker exec $MPC_NAME sh -c 'cd /native; gcovr --add-tracefile "/coverage/coverage-*.json" --html-details /coverage/test-coverage.html'
     docker cp $MPC_NAME:/coverage/ .
+
     httpd_remove
 fi
 
