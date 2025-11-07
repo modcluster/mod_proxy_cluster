8a6358637ef0f3485ea9436b1b4023c2d1edfb3e
Vladimír Chlup <vchlup@ibm.com>
2025-11-07 13:17:01 +0100

    test: Add a workaround for lacking Podman networking
    Unfortunately, custom podman networks do not support rDNS so tomcat
    requests are handled by their IP. This causes our `Require host` to
    not work which leads to test failures.
    
    Upon httpd container start, we try to detect podman and if needed,
    we change `Require host` to `Require ip`.

--- test/httpd/run.sh
+++ test/httpd/run.sh
@@ -11,6 +11,15 @@ else
   exit 1
 fi
 
+# podman networking can't do rDNS, so REMOTE_HOST is an IP address instead of
+# a hostname meaning the `Require host ...` does not work.
+# Because of that, we'll add a `Require ip ...` upon podman detection
+if grep -qc 'search dns.podman' /etc/resolv.conf; then
+    PODMAN_SUBNET=$(sed -rn 's/nameserver ([0-9]+)\.([0-9]+)\.([0-9]+).*/\1\.\2\.\3\./p' /etc/resolv.conf)
+    echo "Podman detected: Changing Require host to Require ip $PODMAN_SUBNET"
+    sed -i "s/Require host \.mod_proxy_cluster_testsuite_net/Require ip ${PODMAN_SUBNET}/" /usr/local/apache2/conf/$FILECONF
+fi
+
 if [ ! -z "$MPC_NAME" ]; then
     sed -i "s/ServerName httpd-mod_proxy_cluster/ServerName ${MPC_NAME}/g" /usr/local/apache2/conf/$FILECONF
 fi
